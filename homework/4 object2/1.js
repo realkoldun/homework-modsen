36fd0d3aaac2cfa8c6dd1e405d5a44507c62aa5c
realkoldun <anton.kitaev.97@gmail.com>
2024-11-21 13:32:57 +0300

    homework object 2 done task 1 spred competed

--- homework/4 object2/1.js
+++ homework/4 object2/1.js
@@ -1,7 +1,14 @@
 function sum() {
     return Array.from(arguments).reduce((sum, e) => {
         return sum + e
-    }, 0)
+    }, 0);
+}
+
+function sum_spread(...args) {
+    return args.reduce((sum, e) => {
+        return sum + e
+    }, 0);
 }
 
 console.log(sum(1, 2, 3, 4));
+console.log(sum_spread(1, 2, 3));
