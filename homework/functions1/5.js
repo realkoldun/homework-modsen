916093e904569277a7e09c35331b8e2a1b24f7ec
realkoldun <anton.kitaev.97@gmail.com>
2024-11-25 17:30:46 +0300

    5.js and 6.js rewrote

--- homework/functions1/5.js
+++ homework/functions1/5.js
@@ -3,4 +3,10 @@ const arraySum = function sum(array) {
     return array.pop() + sum(array);
 }
 
+const arraySum_2 = function sum(array) {
+    if(array.length === 1) return array[0];
+    return array[0] + sum(array.slice(1));
+}
+
 console.log(arraySum([1, 2, 3, 4, 5]));
+console.log(arraySum_2([1, 2, 3, 4, 5]));
--- homework/functions1/6.js
+++ homework/functions1/6.js
@@ -2,12 +2,14 @@ const x = 5;
 
 (() => {
     function findArrayMinMax(arr){
-        let min = arr[0];
+        /*let min = arr[0];
         let max = arr[0];
         for(let i = 1; i < arr.length; i++) {
             if(arr[i] < min) min = arr[i];
             if(arr[i] > max) max = arr[i];
-        }
+        }*/
+        const min = Math.min(...arr);
+        const max = Math.max(...arr);
         return [min, max];
     }
     const array = [];
