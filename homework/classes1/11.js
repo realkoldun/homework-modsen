f096d86dd1950cbd8d44fa203a72de1f7cddc4c4
realkoldun <anton.kitaev.97@gmail.com>
2024-11-27 16:54:48 +0300

    task 11 fixed

--- homework/classes1/11.js
+++ homework/classes1/11.js
@@ -1,9 +1,10 @@
-
 const Vehicle = {
     type: "Автомобиль",
     move: function () {console.log(`${this.type} двигается`) },
 }
 
 const Car = Object.create(Vehicle);
+Car.move = function () {console.log("Машина едет")};
 Car.type = "Машина";
+
 Car.move();
